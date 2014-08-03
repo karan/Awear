4167c07ada8bf32b59024e6e57646ccbcc69d880
Wajahat Siddiqui <agentwaj@gmail.com>
2014-08-03 09:14:45 -0700

    temp: help Aaron Landy with his WebView

--- CMDF/app/src/main/java/com/agentwaj/cmdf/MainActivity.java
+++ CMDF/app/src/main/java/com/agentwaj/cmdf/MainActivity.java
@@ -2,6 +2,9 @@ package com.agentwaj.cmdf;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -10,5 +13,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvMain = (TextView) findViewById(R.id.tvMain);
+        tvMain.setText("Check out my cool new website!");
+
+        WebView wvMain = (WebView) findViewById(R.id.wvMain);
+        wvMain.setWebViewClient(new WebViewClient());
+        wvMain.loadUrl("http://aaronlandy.herokuapp.com/");
     }
 }
