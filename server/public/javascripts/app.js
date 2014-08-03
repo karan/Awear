260b3c4e1c1eb135a514cec1fdd145d5862c8f58
Karan Goel <karan@goel.im>
2014-08-03 09:01:50 -0700

    fix changes

--- server/public/javascripts/app.js
+++ server/public/javascripts/app.js
@@ -19,95 +19,92 @@ $(function() {
   var myFirebaseRef = new Firebase("https://flickering-fire-9434.firebaseio.com/");
 
   if (document.location.hash === "#1") {
-    // 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: 'pmBoUttFKd4',
-          events: {
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
-
+    console.log("in 1");
     myFirebaseRef.on('child_changed', function (snapshot) {
-    var newPost = snapshot.val();
-    if(newPost.UUID === "50522") {
-      $('body').css('background', 'green');
-    }
-    else {
-      $('body').css('background', 'red');
-    }
-    if(newPost.Gesture === "spread"){
-      player.playVideo();
-    }
-  });
-}
+      var newPost = snapshot.val();
+      console.log(newPost);
+      if (newPost.UUID === "50522") {
+        $('body').css('background', 'green');
+        $('#title').text(newPost.Gesture);
+      } else {
+        $('body').css('background', 'red');
+        $('#title').text('');
+      }
+      // if(newPost.Gesture === "spread"){
+      //   player.playVideo();
+      // }
+    });
+    // // 2. This code loads the IFrame Player API code asynchronously.
+    // var tag = document.createElement('script');
+
+    // tag.src = "https://www.youtube.com/iframe_api";
+    // var firstScriptTag = document.getElementsByTagName('script')[0];
+    // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // // 3. This function creates an <iframe> (and YouTube player)
+    // //    after the API code downloads.
+    // var player;
+    // function onYouTubeIframeAPIReady() {
+    //   player = new YT.Player('player', {
+    //     height: '390',
+    //     width: '640',
+    //     videoId: 'pmBoUttFKd4',
+    //     events: {
+    //       'onStateChange': onPlayerStateChange
+    //     }
+    //   });
+    // }
+
+    // // 4. The API will call this function when the video player is ready.
+    // function onPlayerReady(event) {
+    //   event.target.playVideo();
+    // }
+
+    // // 5. The API calls this function when the player's state changes.
+    // //    The function indicates that when playing a video (state=1),
+    // //    the player should play for six seconds and then stop.
+    // var done = false;
+    // function onPlayerStateChange(event) {
+    //   if (event.data == YT.PlayerState.PLAYING && !done) {
+    //     setTimeout(stopVideo, 6000);
+    //     done = true;
+    //   }
+    // }
+    // function stopVideo() {
+    //   player.stopVideo();
+    // }
+  }
 
   if (document.location.hash === "#2") {
+    console.log("in 2");
     myFirebaseRef.on('child_changed', function (snapshot) {
-    var newPost = snapshot.val();
-    if(newPost.UUID === "47826") {
-      $('body').css('background', 'green');
-    }
-    else {
-      $('body').css('background', 'red');
-    }
-    if(newPost.Gesture != "rest"){
+      var newPost = snapshot.val();
+      console.log(newPost);
       $('#title').text(newPost.Gesture);
-    }
-    else {
-     $('#title').text("rest"); 
-    }
-  });
-}
+      if (newPost.UUID === "47826") {
+        $('body').css('background', 'green');
+        $('#title').text(newPost.Gesture);
+      } else {
+        $('body').css('background', 'red');
+        $('#title').text('');
+      }
+    });
+  }
 
   if (document.location.hash === "#3") {
+    console.log("in 3");
     myFirebaseRef.on('child_changed', function (snapshot) {
-    var newPost = snapshot.val();
-    if(newPost.UUID === "28665") {
-      $('body').css('background', 'green');
-    }
-    else {
-      $('body').css('background', 'red');
-    }
-    if(newPost.Gesture != "rest"){
+      var newPost = snapshot.val();
+      console.log(newPost);
       $('#title').text(newPost.Gesture);
-    }
-    else {
-     $('#title').text("rest"); 
-    }
-  });
-}
-  // alert(newPost.Gesture);
+      if (newPost.UUID === "28665") {
+        $('body').css('background', 'green');
+        $('#title').text(newPost.Gesture);
+      } else {
+        $('body').css('background', 'red');
+        $('#title').text('');
+      }
+    });
+  }
   
 });
